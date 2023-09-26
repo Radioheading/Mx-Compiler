b7290505c26c16b5f50802be57d97776ca43e368
Radioheading <BruceLee_SJTU@sjtu.edu.cn>
2023-09-27 00:22:19 +0800

    every story has an end

--- src/Compiler.java
+++ src/Compiler.java
@@ -10,12 +10,9 @@ import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.ParseTree;
 import Util.globalScope;
 
-import java.io.PrintStream;
-
 public class Compiler {
     public static void main(String[] args) throws Exception {
-        // input from file
-        CharStream input = CharStreams.fromFileName("testcases/codegen/e6.mx");
+        CharStream input = CharStreams.fromStream(System.in);
         try {
             RootNode ASTRoot;
             globalScope gScope = new globalScope(null);
@@ -34,11 +31,7 @@ public class Compiler {
             IRBuilder irBuilder = new IRBuilder(gScope);
             irBuilder.visit(ASTRoot);
             new CFG(irBuilder.myProgram).buildCFG();
-            PrintStream out = new PrintStream("output.ll");
-            out.println(irBuilder.myProgram);
             new GlobalToLocal(irBuilder.myProgram).globalTransition();
-            PrintStream err = new PrintStream("opt.ll");
-            err.println(irBuilder.myProgram);
             new DomTreeConstruct(irBuilder.myProgram).work();
             var Mem2Reg = new AllocElimination(irBuilder.myProgram);
             Mem2Reg.eliminateAlloc();
@@ -53,13 +46,11 @@ public class Compiler {
             ASMProgram asmProgram = new ASMProgram();
             new InstSelector(asmProgram).visit(irBuilder.myProgram);
             // new BlockMerger(asmProgram).MergeBlock();
-            // no idea why merging blocks beforehand would have lower efficiency
             new GraphColoring(asmProgram).allocateReg();
             new BlockMerger(asmProgram).MergeBlock();
-            PrintStream asm = new PrintStream("output.s");
-            asm.println(asmProgram);
+            System.out.println(asmProgram);
         } catch (error er) {
-            System.err.println(er);
+            System.err.println(er.toString());
         }
     }
 }
